c84bd121f9345b1ed13f4d2ee91b48d27b8a2776
Vikrant Khedkar <vikrantkhedkar2720@gmail.com>
2021-12-15 19:15:00 +0530

    Ran docker-compose up --build and its working

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.set('visits',0);
 app.get('/',(req, res)=>{
     client.get('visits',(err,visits)=>{
 res.send('Number of visits is'+ visits);
-client.set('visits', parseIntvisits + 1);
+client.set('visits', parseInt(visits) + 1);
     });
 });
 
